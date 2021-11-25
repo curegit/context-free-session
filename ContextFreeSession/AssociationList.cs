9f1f5a744448a49f82dd7addf58878e74f247ef3
curegit <37978051+curegit@users.noreply.github.com>
2021-11-25 10:24:20 +0900

    alist

---
+++ ContextFreeSession/AssociationList.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ContextFreeSession
+{
+    [Serializable]
+    internal class AssociationList<TKey, TValue> : IEnumerable<(TKey, TValue)> where TKey : notnull
+    {
+        private readonly Dictionary<TKey, TValue> dictionary = new();
+
+        private readonly List<TKey> keys = new();
+
+        public void Add(TKey key, TValue value)
+        {
+            dictionary.Add(key, value);
+            keys.Add(key);
+        }
+
+        public void Insert(int index, TKey key, TValue value)
+        {
+            if (index < 0 || index > keys.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index));
+            }
+            else
+            {
+                dictionary.Add(key, value);
+                keys.Insert(index, key);
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<(TKey, TValue)> GetEnumerator()
+        {
+            foreach (var key in keys)
+            {
+                yield return (key, dictionary[key]);
+            }
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return dictionary[key];
+            }
+            set
+            {
+                if (!dictionary.ContainsKey(key))
+                {
+                    Add(key, value);
+                }
+                else
+                {
+                    dictionary[key] = value;
+                }
+            }
+        }
+    }
+}
