a8c8b5cabe413ec94e4af39c6f319587f6c3c303
curegit <37978051+curegit@users.noreply.github.com>
2022-01-14 15:19:36 +0900

    Treeを更新

--- TreeExample/Program.cs
+++ TreeExample/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using ContextFreeSession.Runtime;
 using static ContextFreeSession.Unit;
 
@@ -5,43 +6,48 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
-        var sender = Threading.Fork<Sender_Start, Receiver_Start, Logger_Start>
-(
-    receiver =>
-    {
-        receiver.Do(ReceiverTreeDeleg).Close();
-
-        Eps ReceiverTreeDeleg(Receiver_Tree t)
+        var sender = Threading.Fork<Sender_Start, Receiver_Start, Logger_Start>(receiver =>
         {
-            return t.Branch<Sender, node, leaf>(node => node.Receive<Sender, node>(out var _).Do(ReceiverTreeDeleg).Do(ReceiverTreeDeleg),
-                leaf => leaf.Receive<Sender, leaf>(out var integer)
-            );
-        }
-    },
-    logger =>
-    {
-        logger.Do(LoggerTreeDeleg).Receive<Sender, end>(out var _).Close();
+            var (ch, tree) = receiver.DoFunc(ReceiveTree);
+            Console.WriteLine(tree);
+            ch.Close();
 
-        Eps LoggerTreeDeleg(Logger_Tree t)
+            (Eps, IntTree) ReceiveTree(Receiver_Tree ch)
+            {
+                return ch.BranchFunc<Sender, node, leaf, IntTree>(node =>
+                {
+                    var ch1 = node.Receive<Sender, node>(out var _);
+                    var (ch2, left) = ch1.DoFunc(ReceiveTree);
+                    var (ch3, right) = ch2.DoFunc(ReceiveTree);
+                    return (ch3, new Node(left, right));
+                },
+                leaf =>
+                {
+                    var ch1 = leaf.Receive<Sender, leaf>(out var integer);
+                    return (ch1, new Leaf(integer));
+                });
+            }
+        },
+        logger =>
         {
-            var s = t.Receive<Sender, leaf>(out var integer);
+            logger.Do(LogLeaf).Receive<Sender, end>(out var _).Close();
 
-            Console.WriteLine(integer);
+            Eps LogLeaf(Logger_Tree ch)
+            {
+                var ch1 = ch.Receive<Sender, leaf>(out var integer);
+                Console.WriteLine(integer);
+                return ch1.Do(LogLeafBranch);
+            }
 
-            return s.Do(LoggerTreeDeleg2);
-        }
-
-        Eps LoggerTreeDeleg2(Logger_Tree_ t)
-        {
-            return t.Branch<Sender, leaf, end>(leaf => leaf.Do(LoggerTreeDeleg).Do(LoggerTreeDeleg2), end => end);
-        }
-    }
-);
-
+            Eps LogLeafBranch(Logger_Tree_ ch)
+            {
+                return ch.Branch<Sender, leaf, end>(
+                    leaf => leaf.Do(LogLeaf).Do(LogLeafBranch),
+                    end => end);
+            }
+        });
 
         var tree = new Node(new Leaf(1), new Node(new Leaf(2), new Leaf(3)));
-
         sender.Do(SendTree, tree).Send<Logger, end>(unit).Close();
 
         Eps SendTree(Sender_Tree ch, IntTree tree)
@@ -60,9 +66,19 @@ public class Program
     }
 }
 
-public class IntTree
+public abstract class IntTree
 {
-
+    public override string ToString()
+    {
+        if (this is Node node)
+        {
+            return $"({node.Left} {node.Right})";
+        }
+        else
+        {
+            return ((Leaf)this).Value.ToString();
+        }
+    }
 }
 
 public class Node : IntTree
